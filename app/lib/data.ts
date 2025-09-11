4bd9e20793e16756a767ce99f008a6dc36397eb7
Brady Kelly <brady_kelly@tutamail.com>
2025-09-11 11:10:48 +0200

    Streaming working

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,15 +16,10 @@ const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
